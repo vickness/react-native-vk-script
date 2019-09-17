bbf2923774d545c2c70b163504860796d6cf42dc
weike <weike886@163.com>
2019-09-17 17:59:27 +0800

    修改命令

--- script/init.py
+++ script/init.py
@@ -3,12 +3,31 @@
 
 import sys
 import os
+import shutil
 from libs import tool
 
 # 当前工作目录
 workPath = os.getcwd()
 print workPath
 
-# json文件路径 react-native-upload
-jsonPath = os.path.join(workPath, __file__, "libs/script.json")
-print jsonPath
+# 创建 script.json 文件
+json_in_path = os.path.join(sys.path[0], "libs/script.json")
+json_out_path = os.path.join(workPath, "script.json")
+print json_in_path
+if os.path.isfile(json_out_path) is False:
+    shutil.copy(json_in_path, workPath)
+    tool.log("创建 script.json 文件")
+
+# 安装图片处理库
+try:
+    from PIL import Image, ImageDraw
+except ImportError:
+    tool.log('缺少Image模块，正在安装Image模块，请等待...')
+    success = os.system('python -m pip install Image')
+    if success == 0:
+        tool.log('Image模块安装成功.')
+    else:
+        tool.log('Image安装失败，请手动在终端执行：\'python -m pip install Image\'重新安装.')
+        quit()
+
+# 安装xcode解析库
